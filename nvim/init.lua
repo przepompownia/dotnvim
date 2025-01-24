f0624a2d30a065cad3d08e166eec0a9604c4d8b5
przepompownia <przepompownia@users.noreply.github.com>
2025-01-24 12:54:03 +0100

    init: exclude system paths from runtimepath

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,12 @@ vim.g.pluginDirs = vim.iter({
   return vim.fs.joinpath(vim.fn.stdpath('config'), 'pack', group, 'opt')
 end):totable()
 
+for _, path in ipairs(vim.opt.runtimepath:get()) do
+  if vim.startswith(path, '/etc/') or vim.startswith(path, '/usr/') then
+    vim.opt.runtimepath:remove(path)
+  end
+end
+
 vim.loader.enable()
 
 ---@type NvimPlugins
